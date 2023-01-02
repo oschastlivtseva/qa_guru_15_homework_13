0cc0e3d161122230fd9d6a7ed6b3a917d9683095
Olga Schastlivtseva <olga.schastlivtseva@yandex.ru>
2023-01-03 01:02:21 +0400

    add tests

--- src/test/java/guru/qa/tests/TestBase.java
+++ src/test/java/guru/qa/tests/TestBase.java
@@ -27,15 +27,18 @@ public class TestBase {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("browserName", "chrome");
-        capabilities.setCapability("browserVersion", "100.0");
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
 
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "770x1024";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+        Configuration.browserSize = System.getProperty("browser_size", "770x1024");
+        String remoteUrl = System.getProperty("remote_url");
+        if (!remoteUrl.equals("")) {
+            Configuration.remote = remoteUrl;
+        }
     }
 
     @AfterAll
